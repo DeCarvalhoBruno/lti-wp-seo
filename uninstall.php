ad2a484170bf0352305512e395d7cdf9f3838458
Bruno De Carvalho <decarvalho.bruno@free.fr>
2015-05-11 10:46:05 +0200

    Optimized PNGs Made sure plugin related user profile data was removed upon uninstall

--- uninstall.php
+++ uninstall.php
@@ -6,7 +6,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit();
 }
 
+//Deleting plugin options
 delete_option('lti_seo_options');
+
+//Deleting extra post information added by the plugin
 delete_post_meta_by_key( 'lti_seo' );
+
+//Deleting user profile fields created by the plugin
+$user_meta_keys = array('lti_public_email','lti_job_title','lti_work_longitude','lti_work_latitude','lti_twitter_username','lti_facebook_id','lti_facebook_url','lti_gplus_url','lti_instagram_url','lti_youtube_url','lti_linkedin_url','lti_myspace_url');
+
+foreach($user_meta_keys as $key){
+    delete_metadata( 'user', 0, $key, '', true );
+}
+
 
 ?>
